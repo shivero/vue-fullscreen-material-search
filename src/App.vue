6c45a54f4a63d911df342d1bd14636e59e5a49c8
sh1vero <grzesiekdziubek@gmail.com>
2018-07-21 00:02:29 +0200

    moved entire project to vscode - fixed eslint errors

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-    
+
     <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: "Roboto", Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
--- src/main.js
+++ src/main.js
@@ -1,22 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import App from "./App";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Vue from 'vue';
+import App from './App';
 
 library.add(faSearch, faTimesCircle);
 
-Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
-Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
+  el: '#app',
   components: { App },
-  template: "<App/>"
+  template: '<App/>',
 });
